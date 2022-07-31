0eb3b1b016091bca61408feb98861f6fd3622862
Jan Zelinka <jan.zelinka12@gmail.com>
2022-07-31 20:32:19 +0200

    initial commit

---
+++ Core/Entities/Product.cs
@@ -0,0 +1,6 @@
+namespace Core.Etities {
+    public class Product {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,13 @@
+using Core.Etities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data {
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
